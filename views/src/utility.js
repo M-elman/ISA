6a927116c7feb7b1858bf29850a49594af04443f
M-elman <M-elman@users.noreply.github.com>
2017-11-12 18:18:49 +0100

    Check taxcode added

--- views/src/utility.js
+++ views/src/utility.js
@@ -11,15 +11,24 @@ function checkPwdStrenght()  {
 };
 
 function checkDate()  {
+    var formatRegExp=/\d{4}-\d{2}-\d{2}/; /*slash indica l'inizio e la fine della regexp, \d indica un digit, ripetuto 4 o due volte a seconda del numero indicato fra graffe. Tale espressione regolare controlla solo il formato desiderato della data, mentre la validità della stessa viene controllata dai metodi seguenti*/
+    if (formatRegExp.test(document.getElementById("birthdate").value)==false){
+        document.getElementById("birthdate").setCustomValidity("Please use the format yyyy-mm-dd");
+        document.getElementById("birthdate").value="";
+    }
+    else{
     if(isNaN(Date.parse(document.getElementById("birthdate").value))==true) {
-        document.getElementById("birthdate").setCustomValidity("Please input a date");
+        document.getElementById("birthdate").setCustomValidity("Please input a valid date");
+        document.getElementById("birthdate").value="";
     }
     else{
         var birthday= new Date(document.getElementById("birthdate").value);
-        if(birthday < new Date("01/01/1900") || birthday > new Date() ){
-        document.getElementById("birthdate").setCustomValidity("Please select a valid date, between 01/01/1900 and today");
+        if(birthday < new Date("1900-01-01") || birthday > new Date() ){
+        document.getElementById("birthdate").setCustomValidity("Please select a date between 1900-01-01 and today");
+        document.getElementById("birthdate").value="";
     }
-    }  
+    }
+    }
 };
 
 function checkUsername()  {
@@ -36,19 +45,46 @@ xhttp.send(JSON.stringify({username: document.getElementById("username").value})
 };
 
 function checkTaxCode() {
-    var man;
-    if(document.getElementById("gender").value==="M") man=true;
-    else man=false;
     
-    var personCF = new CodiceFiscale({
+    var generality = {
     name: document.getElementById("name").value,
-    lastname: document.getElementById("surname").value,
-    day: new Date(document.getElementById("birthdate").value).getDay(),
+    surname: document.getElementById("surname").value,
+    gender: document.getElementById("gender").value,
+    day: new Date(document.getElementById("birthdate").value).getDate(),
     month: new Date(document.getElementById("birthdate").value).getMonth() + 1,
     year: new Date(document.getElementById("birthdate").value).getFullYear(),
-    isMale: man,
-    communeName: document.getElementById("birthTown").value
-});
-   // console.log(personCF.taxCode())
+    birthplace: document.getElementById("birthTown").value,
+    birthplace_provincia: document.getElementById("birthProvince").value.toUpperCase()
+};
+   if(CodiceFiscale.compute(generality) !== document.getElementById("taxCode").value.toUpperCase()){
+        document.getElementById("taxCode").setCustomValidity("This field was incorrect");
+        document.getElementById("taxCode").value="";
+    }
+    
+}
+
+function computeTaxCode() {
+    if (document.getElementById("name").value != null && document.getElementById("name").value !="" &&
+       document.getElementById("surname").value != null && document.getElementById("surname").value !="" &&
+       document.getElementById("gender").value != null && document.getElementById("gender").value !="" &&
+       document.getElementById("birthdate").value != null && document.getElementById("birthdate").value !="" &&
+       document.getElementById("birthTown").value != null && document.getElementById("birthTown").value !="" &&
+       document.getElementById("birthProvince").value != null && document.getElementById("birthProvince").value !=""){
+        
+        var generality = {
+        name: document.getElementById("name").value,
+        surname: document.getElementById("surname").value,
+        gender: document.getElementById("gender").value,
+        day: new Date(document.getElementById("birthdate").value).getDate(),
+        month: new Date(document.getElementById("birthdate").value).getMonth() + 1,
+        year: new Date(document.getElementById("birthdate").value).getFullYear(),
+        birthplace: document.getElementById("birthTown").value,
+        birthplace_provincia: document.getElementById("birthProvince").value.toUpperCase()
+        };
+        
+        document.getElementById("taxCode").value=CodiceFiscale.compute(generality);
+
+    }
+    
     
 }

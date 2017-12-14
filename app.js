1625c98df38ff3a1cb84245680559290387a5bea
M-elman <M-elman@users.noreply.github.com>
2017-12-14 18:31:14 +0100

    modification

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var port = process.env.PORT || 5000;
 
 
 
-server.listen(process.env.PORT);
+server.listen(PORT);
 io.sockets.on('connection', function(socket){
   socket.on('storeClientInfo', function (data) {
       var clientInfo = new Object();
